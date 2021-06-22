29de6b8d0ef202f42936597d00d816fe403d1e80
Taís Foltran Orsini <taisfoltranorsini.com>
2021-06-22 08:52:11 -0300

    html done missing css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <nav></nav>
+      <header>
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Awesome!</button>
       </header>
+      <section className="row align-items-center">
+
+        <div className="col">
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div className="col">
+          <h2>Components</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div className="col">
+          <h2>Single-Way</h2>
+          <p>A set o immutable values are passed to their component's.</p>
+        </div>
+        <div className="col">
+          <h2>JSX</h2>
+          <p>Statically-typed, designed to run on modern browsers.</p>
+        </div>
+        
+      </section>
     </div>
   );
 }
